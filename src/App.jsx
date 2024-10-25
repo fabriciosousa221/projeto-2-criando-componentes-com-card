2d772b9c4f69f8b0c994312cd5b4e0078e3c5f0a
Fabrício Sousa <151534686+fabriciosousa221@users.noreply.github.com>
2024-10-25 16:03:42 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Card from './components/Card'
+
+export default function App() {
+    return (
+        <div>
+            <Card />
+            <h1>Pôster: Star Wars (1997)</h1>
+
+            <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
+            <button>Compre </button>
+        </div>
+        
+    )
+}
